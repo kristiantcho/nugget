d0963abea914762df4f5c614d194b69383ca47f9
Kristian Tchiorniy <41946704+kristiantcho@users.noreply.github.com>
2025-10-06 15:29:01 +0200

    req update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nugget",
-    version="0.1.0",
+    version="2.0",
     description="NeUtrino experiement Geometry optimization and General Evaluation Tool",
     author="",
     author_email="",
